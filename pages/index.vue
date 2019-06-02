28e1640f8e5fe09a44b2e8f0caecadf8d684bef8
bfreeds <5316367+bfreeds@users.noreply.github.com>
2019-06-02 00:27:38 -0500

    Add avatar image

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,10 @@
 // home page
 <template lang="md">
+
+<div class="container">
+<Avatar class='photo' />
+
+
 # Welcome Message
 # Who Am I
 I'm Brandyn, a geographer who does web development and design with a ❤️ for maps and community. 
@@ -14,17 +19,26 @@ I'm Brandyn, a geographer who does web development and design with a ❤️ for 
 1.  Draw people to read the blog
 1.  Draw people to look at the portfolio  
 
+</div>
 </template>
 
 <script>
+import Avatar from '../components/Avatar'
+
+export default {
+  components: {
+    Avatar
+  }
+}
 </script>
 
 <style lang="scss" scoped>
 .container {
   margin: 0 auto;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+  padding: 0 30px;
+  // display: flex;
+  // flex-direction: column;
+  // align-items: flex-start;
 }
 
 h1 {
@@ -32,5 +46,10 @@ h1 {
   line-height: 2.6rem;
   color: $gray6;
   font-weight: 200;
+}
+
+.photo {
+  shape-outside: circle(50%);
+  float: right;
 }
 </style>
