a856186010c9f389f857076084501e29cb14fe9e
bfreeds <5316367+bfreeds@users.noreply.github.com>
2019-06-22 22:09:24 -0500

    Reorder footer content

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,5 @@
 <template>
   <footer>
-    <p>Made with Vue and NuxtJS</p>
     <div class="profile-buttons">
       <!-- twitter -->
       <a href="https://twitter.com/brandynfriedly"><font-awesome :icon="['fab', 'twitter']" class="fa-icon" /></a>
@@ -8,10 +7,11 @@
       <a href="https://github.com/bfreeds"><font-awesome :icon="['fab', 'github']" class="fa-icon" /></a>
       <!-- linkedin -->
       <a href="https://www.linkedin.com/in/brandyn-friedly-2b154b187/">    <font-awesome :icon="['fab', 'linkedin']" class="fa-icon" /></a>
-
       <!-- instagram -->
       <a href="https://www.instagram.com/brandyn.friedly/">      <font-awesome :icon="['fab', 'instagram']" class="fa-icon" /></a>
     </div>
+
+    <p>Made with Vue and NuxtJS</p>
   </footer>
 </template>
 
@@ -26,7 +26,7 @@ footer {
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
+  justify-content: space-around;
 }
 
 .profile-buttons {
